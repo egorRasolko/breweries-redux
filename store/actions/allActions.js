50f2212a5d39f235318a323947483407de160256
egorRasolko <83365207+egorRasolko@users.noreply.github.com>
2021-08-23 15:52:25 +0300

    Delete store directory

--- store/actions/allActions.js
+++
@@ -1,6 +0,0 @@
-export const SAVE_DATA = 'sd';
-export const SAVE_SEARCH_DATA = 'ssd';
-export const CLEAR_SEARCH = 'ccsd';
-export const LOAD_DATA = 'ld';
-export const SEARCH_DATA = 'sd'
-export const SET_ERROR = 'ser';
--- store/initialState.js
+++
@@ -1,6 +0,0 @@
-export const initialState = {
-    items: [],
-    error: null,
-    searchItems: [],
-    useSearch: false,
-};
