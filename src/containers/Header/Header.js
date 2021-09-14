23162bb141578149328eb8c15cb433e2fd2e4f06
egor <egor_rasolko@mail.ru>
2021-09-14 20:20:03 +0300

    css-modules

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { bindActionCreators } from 'redux';
+import classes from './Header.module.css';
+import { connect } from 'react-redux'
+import {searchData, clearSearch} from './Actions/pageAction.js';
+
+class Header extends React.Component{
+constructor(props){
+    super(props);
+    this.searchItems = this.searchItems.bind(this);
+    this.clearSearchClick = this.clearSearchClick.bind(this);
+}
+
+ 
+searchItems(){
+  let inputValue = document.querySelector('.input-text').value;
+  console.log(`searching <${inputValue}>`)
+  this.props.searchData(inputValue)
+}
+
+clearSearchClick() {
+  this.props.clearSearch();
+}
+
+
+render() {
+     
+    return(
+        <div>
+        <h1 className={classes.title}>
+        AWESOME breweries
+      </h1>
+       <div>
+       <input type='text' className={classes.input_text} ></input>
+       <button onClick={this.searchItems}>Search</button>
+       <button onClick={this.clearSearchClick}>Clear search</button>
+       </div>
+       </div>
+    )           
+  }
+}
+
+const mapStateToProps = state => {
+ return { errpr: state.error }
+}
+
+function mapDispatchToProps(dispatch) {
+        return {
+          dispatch,
+          ...bindActionCreators({searchData, clearSearch}, dispatch),
+        };
+        
+	}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
---
+++ src/containers/beerBlock/beerBlock.js
@@ -0,0 +1,39 @@
+import React from'react'
+import classes from './beerBlock.module.css';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+
+function yearsDiff(dt) { 
+    let tmp = new Date(dt)
+    let DifMs = Date.now() - tmp;
+    let deltaDt = new Date(DifMs); // miliseconds from epoch
+    return Math.abs(deltaDt.getUTCFullYear() - 1970);
+  }
+
+class BeerBlock extends React.Component {
+   
+    render() {
+        return (
+            <div className={classes.framegent}>
+                <h3 className={classes.titleh3}>{this.props.name}</h3>
+                <div className='Map'>
+                {this.props.latitude ? <YMaps>
+                    <Map defaultState={{ center: [this.props.latitude, this.props.longitude], zoom: 9,  width:240, height:120 }}>
+                        <Placemark defaultGeometry={[this.props.latitude, this.props.longitude]} />
+                    </Map>
+                    </YMaps> : <p className={classes.noMap}> MAP IS NOT AVALIABLE </p>}
+                </div>
+                <div className={classes.contentAlign}>
+                    { this.props.website_url ? 
+                        <a className={classes.website} href={this.props.website_url} target='_blank' rel='noreferrer'>{this.props.website_url}</a> 
+                    :
+                        <p className={classes.website}>No link</p>
+                    }
+                    <p className={classes.lastupd}>Last update:{ yearsDiff(this.props.updated_at) } years ago</p>
+                </div>
+            </div>                        
+        )
+    }
+};
+
+
+export default BeerBlock
